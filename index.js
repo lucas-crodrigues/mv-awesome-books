9cb7469ad2f26bb652b714ebf1eae14d6238ebfa
alvarorf <aaruizf@unal.edu.co>
2022-04-25 17:18:11 -0400

    Fix issue with addBook and removeBook functions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const books = [
+let books = [
   {
     title: 'Title1',
     author: 'author1'
@@ -10,7 +10,7 @@ const books = [
   {
     title: 'Title3',
     author: 'author3'
-  },
+  }
 ];
 class Book {
   constructor(title, author) {
@@ -18,29 +18,34 @@ class Book {
     this.author = author;
   }
 
-  static addBook = (title, author) => {
-    books.push({ title, author });
+  static addBook = () => {
+    const ititle = document.getElementById('addTitle').value;
+    const iauthor = document.getElementById('addAuthor').value;
+    books.push({ title: ititle, author: iauthor });
+    this.renderBooks();
   }
 
-  static removeBook = (title) => {
+  static removeBook = (e) => {
+    const button = e.target;
+    const title = button.id.split('_')[1];
     books = books.filter((book) => book.title !== title);
-    console.log(books)
-    return books
+    this.renderBooks();
+    return books;
   }
 
   static renderBooks() {
-    // let books = 
     const booksContainer = document.getElementById('books-container');
     booksContainer.innerHTML = this.markupAllBooks();
+    Book.addEventListeners();
   }
-  
+
   static markupAllBooks() {
     let allBooksHTML = ``;
-    books.forEach((book, index) => {
+    books.forEach((book) => {
       allBooksHTML += `<div class="book" id="${book.title}">
                         <p class="title">${book.title}</p>
                         <p class="author">${book.author} </p>
-                        <button type="button" id="r_${book.title}">Remove</button>
+                        <button type="button" id="r_${book.title}">"Remove</button>
                         <hr>
                       </div>`;
     });
@@ -50,12 +55,14 @@ class Book {
 
   static addEventListeners = () => {
     const addButton = document.getElementById('addButton');
-    const addTitle = document.getElementById('addTitle');
-    const addAuthor = document.getElementById('addAuthor');
-    addButton.addEventListener('click', () => { this.addBook(addTitle.value, addAuthor.value) })
+
+    addButton.addEventListener('click', this.addBook );
+    let removeButtons = Array.from(document.querySelectorAll('.book button'));
+    removeButtons.forEach((button) => {
+      button.addEventListener('click', this.removeBook);
+    })
   }
 
 }
 
-Book.addEventListeners();
 Book.renderBooks();
